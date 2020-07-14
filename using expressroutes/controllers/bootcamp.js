462d5543fb5e15174ebffd23958284d45243d664
Suvish23 <67574702+Suvish23@users.noreply.github.com>
2020-07-15 00:13:25 +0530

    Update bootcamp.js

---
+++ using expressroutes/controllers/bootcamp.js
@@ -0,0 +1,50 @@
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamp');
+const router = express.Router();
+router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+  
+  //to work this,, we need to export the router....
+module.exports = router;
+//instead of this simple method using controller.....
+
+// router.get('/:id', (req, res) => {
+//   res.status(200).json({
+//     sucess: true,
+//     message: 'Show all bootcamps',
+//   });
+// });
+// router.get('/', (req, res) => {
+//   res.status(200).json({
+//     sucess: true,
+//     message: `get bootcamp ${req.params.id}`,
+//   });
+// });
+// router.put('/:id', (req, res) => {
+//   res.status(200).json({
+//     sucess: true,
+//     message: `Update bootcamp ${req.params.id}`,
+//   });
+// });
+// router.post('/', (req, res) => {
+//   res.status(200).json({
+//     sucess: true,
+//     message: 'create a new bootcamp',
+//   });
+// });
+// router.delete('/:id', (req, res) => {
+//   res.status(200).json({
+//     sucess: true,
+//     message: `Delete bootcamp ${req.params.id}`,
+//   });
+// });
+// 
